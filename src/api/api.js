be85d976b38dcb9c711fb3187b5624775239df07
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-23 02:43:14 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,8 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`; // Добавляем Bearer
+    console.log('Токен добавлен в заголовок:', token); // Логируем
   }
   return config;
 });
