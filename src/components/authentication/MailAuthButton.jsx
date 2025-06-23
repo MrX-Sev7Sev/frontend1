ac60cae1dd2e31ce03d76d0bfc1e928ca0e6c298
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-23 15:33:19 +0500

    Update MailAuthButton.jsx

--- src/components/authentication/MailAuthButton.jsx
+++ src/components/authentication/MailAuthButton.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
-import api from '../../../api/api';
+import api from '../../../api'
 import axios from 'axios';
 
 export const MailAuthButton = () => {
