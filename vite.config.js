cdf3521fecb5483ea84ef603056584ed1250888e
Арсений <119298545+MrX-Sev7Sev@users.noreply.github.com>
2025-06-23 15:39:06 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context')
+    }
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'https://eventmaster-0w4v.onrender.com',
+        target: 'https://eventmaster2.onrender.com',
         changeOrigin: true,
         secure: false,
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 });
